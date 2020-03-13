7141ff68ddd332e5ed4a6a4191b86ce9abf8e9a0
arsenii-152 <55283004+arsenii-152@users.noreply.github.com>
2020-03-13 22:19:50 +0300

    rekt

---
+++ index.js
@@ -0,0 +1,15 @@
+var object = document.querySelector(".seconder")
+function secondRotate() {
+  object.style.transform = 'rotate(45deg)'
+  }
+function secondRotateTwice() {
+  object.style.transform = 'rotate(90deg)'
+}
+var clicker = document.querySelector(".clickl")
+function turnItOn () {
+  clicker.style.transform = 'rotate(90deg)'
+}
+let button = document.querySelector('.next')
+function btnPressed() {
+  button.style.backgroundColor = "#A60000"
+}
