29c76f382f3488004f4773fcf8de21722f3a3925
arsenii-152 <55283004+arsenii-152@users.noreply.github.com>
2020-03-14 18:46:00 +0300

    почти готова первая страница

--- js/index.js
+++ js/index.js
@@ -1,12 +1,4 @@
-var object = document.querySelector(".seconder")
 
-function secondRotate() {
-  object.style.transform = 'rotate(45deg)'
-}
-
-function secondRotateTwice() {
-  object.style.transform = 'rotate(90deg)'
-}
 
 let igniteAnimation = anime({
   targets: '.clickl ',
@@ -20,7 +12,8 @@ let igniteBtn = document.querySelector('.clickl')
 igniteBtn.onclick = igniteAnimation.play
 
 let screenl = document.querySelector('.screen')
-
+let danger = document.querySelector('.danger')
+let text = document.querySelector('.text')
 let igniteAnimation1 = anime({
     targets: '.clickr',
     translateY: -50,
@@ -30,16 +23,47 @@ let igniteAnimation1 = anime({
 })
 document.querySelector('.clickr').addEventListener('click', function() {
   screenl.style.background = "black"
+  text.style.opacity = 1
+  danger.style.background = "#2AB927" 
 });
 
 let igniteBtn1 = document.querySelector('.clickr')
 igniteBtn1.onclick = igniteAnimation1.play
 
-let nextAnimationFirst = anime({
-  targets: '.firstpanel',
-  opacity: 0,
-  autoplay: false
+let body = document.querySelector('.body')
+let off = document.querySelector('.off')
+let turnOff = anime({
+    targets: '.seconder',
+    rotate: 1,
+    duration: 200,
+    autoplay: false
+})
+off.onclick = turnOff.play
+document.querySelector('.off').addEventListener('click', function() {
+  body.style.background = `black`
+});
+let becomeStable = anime({
+    targets: '.seconder',
+    rotate: 45,
+    duration: 200,
+    autoplay: false
+})
+document.querySelector('.stable').addEventListener('click', function() {
+  body.style.background = `rgb(${anime.random(50,255)},${anime.random(50,255)},${anime.random(50,255)})`
+});
+
+
+let stable = document.querySelector('.stable')
+stable.onclick = becomeStable.play
+let becomeDynamic = anime({
+    targets: '.seconder',
+    rotate: 90,
+    duration: 200,
+    autoplay: false
 })
 
-let nextOne = document.querySelector('.next')
-nextOne.onclick = nextAnimationFirst.play
+document.querySelector('.dynamic').addEventListener('click', function() {
+  body.style.background = "linear-gradient(90deg, #6A3093 5.44%, #A044FF 100%)"
+});
+let dynamic = document.querySelector('.dynamic')
+dynamic.onclick = becomeDynamic.play
