ef06710b914f76e1432b88849c52d096ac7452dc
Студент <student@mp269w388149.local>
2020-03-14 14:10:29 +0300

    xd

--- js/index.js
+++ js/index.js
@@ -1,39 +1,45 @@
 var object = document.querySelector(".seconder")
+
 function secondRotate() {
   object.style.transform = 'rotate(45deg)'
-  }
+}
+
 function secondRotateTwice() {
   object.style.transform = 'rotate(90deg)'
 }
-let igniteAnimation = anime ({
+
+let igniteAnimation = anime({
   targets: '.clickl ',
   translateY: -50,
   rotate: 180,
   duration: 50,
   autoplay: false
 })
+
 let igniteBtn = document.querySelector('.clickl')
-  igniteBtn.onclick = igniteAnimation.play
+igniteBtn.onclick = igniteAnimation.play
 
 let screenl = document.querySelector('.screen')
-  let igniteAnimation1 = anime ({
+
+let igniteAnimation1 = anime({
     targets: '.clickr',
     translateY: -50,
     rotate: 180,
     duration: 50,
     autoplay: false
-  })
-  document.querySelector('.clickr').addEventListener('click',
-  function() {
-  screenl.style.backgroundColor = 'black'
+})
+document.querySelector('.clickr').addEventListener('click', function() {
+  screenl.style.background = "black"
 });
-let igniteBtn1 = document.querySelector('.clickr')
-  igniteBtn1.onclick = igniteAnimation1.play
 
-let nextAnimationFirst = anime ({
+let igniteBtn1 = document.querySelector('.clickr')
+igniteBtn1.onclick = igniteAnimation1.play
+
+let nextAnimationFirst = anime({
   targets: '.firstpanel',
   opacity: 0,
   autoplay: false
 })
+
 let nextOne = document.querySelector('.next')
-  nextOne.onclick = nextAnimationFirst.play
+nextOne.onclick = nextAnimationFirst.play
