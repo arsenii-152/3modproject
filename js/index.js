ab4d9bfa29f18852a694df1807b8c9f3bb4ce922
Студент <student@mp269w388149.local>
2020-03-14 13:16:06 +0300

    prerekt

---
+++ js/index.js
@@ -0,0 +1,35 @@
+var object = document.querySelector(".seconder")
+function secondRotate() {
+  object.style.transform = 'rotate(45deg)'
+  }
+function secondRotateTwice() {
+  object.style.transform = 'rotate(90deg)'
+}
+let igniteAnimation = anime ({
+  targets: '.clickl ',
+  translateY: -50,
+  rotate: 180,
+  duration: 50,
+  autoplay: false
+})
+let igniteBtn = document.querySelector('.clickl')
+  igniteBtn.onclick = igniteAnimation.play
+
+
+  let igniteAnimation1 = anime ({
+    targets: '.clickr',
+    translateY: -50,
+    rotate: 180,
+    duration: 50,
+    autoplay: false
+  })
+let igniteBtn1 = document.querySelector('.clickr')
+  igniteBtn1.onclick = igniteAnimation1.play
+
+let nextAnimationFirst = anime ({
+  targets: '.firstpanel',
+  opacity: 0,
+  autoplay: false
+})
+let nextOne = document.querySelector('.next')
+  nextOne.onclick = nextAnimationFirst.play
